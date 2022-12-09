489858424a5655ce4bce82eacc727efbf314b4a3
Max Roberts-Dear <maxroberts-dear@MacBook-Pro-6.local>
2022-12-09 18:51:01 +0000

    Adding animated tab button - pinterest inspired

--- Zable/Zable.tsx
+++ Zable/Zable.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { Dimensions, View, StyleSheet } from "react-native";
+import {
+  Dimensions,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedbackBase,
+  Pressable,
+  TouchableWithoutFeedback,
+} from "react-native";
 import Animated, {
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useSharedValue,
   interpolateColor,
+  withTiming,
 } from "react-native-reanimated";
 
 import { products } from "./Model";
 import Card, { CARD_HEIGHT } from "./Card";
 import Products from "./Products";
-
-const { width } = Dimensions.get("window");
+import { Ionicons } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
   slider: { height: CARD_HEIGHT },
 });
+
+const MENU_ITEM_SIZE = 60;
+const { width, height } = Dimensions.get("window");
 
 const Zable = () => {
   const translateX = useSharedValue(0);
@@ -32,6 +42,43 @@ const Zable = () => {
     ) as string;
     return { flex: 1, backgroundColor };
   });
+
+  const isToggled = useSharedValue(false);
+  const toggleMenu = () => {
+    isToggled.value = !isToggled.value;
+  };
+
+  const menuItemLeft = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: isToggled.value ? withTiming(-45) : withTiming(0),
+      },
+      {
+        translateX: isToggled.value ? withTiming(-60) : withTiming(0),
+      },
+    ],
+    opacity: isToggled.value ? withTiming(1) : withTiming(0),
+  }));
+  const menuItemCenter = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: isToggled.value ? withTiming(-70) : withTiming(0),
+      },
+    ],
+    opacity: isToggled.value ? withTiming(1) : withTiming(0),
+  }));
+  const menuItemRight = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: isToggled.value ? withTiming(-45) : withTiming(0),
+      },
+      {
+        translateX: isToggled.value ? withTiming(60) : withTiming(0),
+      },
+    ],
+    opacity: isToggled.value ? withTiming(1) : withTiming(0),
+  }));
+
   return (
     <Animated.View style={style}>
       <View style={styles.slider}>
@@ -48,6 +95,91 @@ const Zable = () => {
           ))}
         </Animated.ScrollView>
         <Products x={translateX} />
+      </View>
+      <View
+        style={{
+          width,
+          height: height / 2,
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <Animated.View
+          style={[
+            {
+              width: MENU_ITEM_SIZE,
+              position: "absolute",
+              bottom: 40,
+              alignSelf: "center",
+              borderRadius: MENU_ITEM_SIZE / 2,
+              height: MENU_ITEM_SIZE,
+            },
+            menuItemLeft,
+          ]}
+        >
+          <Ionicons
+            name={"pie-chart"}
+            size={MENU_ITEM_SIZE}
+            color={"white"}
+            style={{ alignSelf: "center" }}
+          />
+        </Animated.View>
+        <Animated.View
+          style={[
+            {
+              width: MENU_ITEM_SIZE,
+              position: "absolute",
+              bottom: 40,
+              alignSelf: "center",
+              borderRadius: MENU_ITEM_SIZE / 2,
+              height: MENU_ITEM_SIZE,
+            },
+            menuItemCenter,
+          ]}
+        >
+          <Ionicons
+            name={"cash"}
+            size={MENU_ITEM_SIZE}
+            color={"white"}
+            style={{ alignSelf: "center" }}
+          />
+        </Animated.View>
+        <Animated.View
+          style={[
+            {
+              width: MENU_ITEM_SIZE,
+              position: "absolute",
+              bottom: 40,
+              alignSelf: "center",
+              borderRadius: MENU_ITEM_SIZE / 2,
+              height: MENU_ITEM_SIZE,
+            },
+            menuItemRight,
+          ]}
+        >
+          <Ionicons
+            name={"bar-chart-outline"}
+            size={MENU_ITEM_SIZE}
+            color={"white"}
+            style={{ alignSelf: "center" }}
+          />
+        </Animated.View>
+        <Pressable
+          style={{
+            width: MENU_ITEM_SIZE * 1.4,
+            bottom: 30,
+            borderRadius: (MENU_ITEM_SIZE * 1.4) / 2,
+            height: MENU_ITEM_SIZE * 1.4,
+          }}
+          onPress={toggleMenu}
+        >
+          <Ionicons
+            name={"add-circle"}
+            size={MENU_ITEM_SIZE * 1.4}
+            color={"white"}
+            style={{ alignSelf: "center" }}
+          />
+        </Pressable>
       </View>
     </Animated.View>
   );
